e95cddcd894cc5c273887c76ded2efb85d652f16
mannasugo <mannasugo@gmail.com>
2024-02-29 20:46:09 +0300

    ...

--- tools.js
+++ tools.js
@@ -268,9 +268,9 @@ class Tools {
 
 		let Pairs = [{
 			gas: 7.5/100,
-			pair: [`btc/fdusd`, [62977.24, 63134.13]],
-			ts_a: new Date(`2024-02-29 17:04`).valueOf(), 
-			ts_z: new Date(`2024-02-29 17:08`).valueOf()
+			pair: [`btc/fdusd`, [62371.24, 62573.72]],
+			ts_a: new Date(`2024-02-29 19:27`).valueOf(), 
+			ts_z: new Date(`2024-02-29 19:31`).valueOf()
 		}];
 
 		//writeFileSync(`json/pairs.json`, this.coats(Pairs));
