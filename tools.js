5ec2e1f7770ba0c8d3ffc52407b064549a4de463
mannasugo <mannasugo@gmail.com>
2024-06-11 10:50:20 +0300

    ...

--- tools.js
+++ tools.js
@@ -271,7 +271,7 @@ class Tools {
 
 	execute (Raw) {
 
-		let ts = new Date(`2024-06-11 10:23`).valueOf();
+		let ts = new Date(`2024-06-11 10:42`).valueOf();
 
 		let md = createHash(`md5`).update(`${ts}`, `utf8`).digest(`hex`)
 		
@@ -280,8 +280,8 @@ class Tools {
 			ilk: `market`,
 			md: md,
 			mug: hold,
-			pair: [[`eur`, `usd`], [0, 1]],
-			side: `buy`,
+			pair: [[`eur`, `usd`], [0, 1/70063.53]],
+			side: `sell`,
 			ts: ts,
 			ts_z: ts
 		};
